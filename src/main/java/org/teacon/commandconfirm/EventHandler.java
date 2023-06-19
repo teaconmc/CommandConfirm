a6f566ebe757aa3cac349a3e83e2dbe07c48b72c
SeraphJACK <seraphjack@outlook.com>
2023-06-19 20:35:18 +0800

    Add localization for confirmation message

--- src/main/java/org/teacon/commandconfirm/EventHandler.java
+++ src/main/java/org/teacon/commandconfirm/EventHandler.java
@@ -7,8 +7,15 @@ import net.minecraftforge.event.CommandEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
+import java.util.Map;
+
 @Mod.EventBusSubscriber(modid = CommandConfirm.ID)
 public class EventHandler {
+    public static final Map<String, String> TEMPLATES = Map.of(
+            "en_us", "WARNING: %d entities will be affected by this command! Execute again to proceed.",
+            "zh_cn", "警告：%d 个实体将被该命令影响！再次执行以继续。"
+    );
+
     @SubscribeEvent
     public static void onCommand(CommandEvent event) {
         var rawCommand = event.getParseResults().getReader().getString();
@@ -35,8 +42,9 @@ public class EventHandler {
 
         if (count > Config.warningLimit.get()) {
             event.setCanceled(true);
-            source.sendFailure(Component.literal("WARNING: " + count + "entities will be affected by this command. " +
-                    "Execute again to proceed."));
+            var lang = player.getLanguage();
+            if (!TEMPLATES.containsKey(lang)) lang = "en_us";
+            source.sendFailure(Component.literal(String.format(TEMPLATES.get(lang), count)));
         }
     }
 }
