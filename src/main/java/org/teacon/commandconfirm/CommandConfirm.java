1d8236799cfa8b7b7b0dac45efffc017ef545678
3TUSK <git@tritusk.info>
2024-08-26 23:07:31 -0700

    Switch logger to slf4j

--- src/main/java/org/teacon/commandconfirm/CommandConfirm.java
+++ src/main/java/org/teacon/commandconfirm/CommandConfirm.java
@@ -3,14 +3,14 @@ package org.teacon.commandconfirm;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Mod(CommandConfirm.ID)
 public final class CommandConfirm {
     public static final String ID = "commandconfirm";
 
-    public static final Logger logger = LogManager.getLogger();
+    public static final Logger logger = LoggerFactory.getLogger(CommandConfirm.class);
 
     public CommandConfirm(ModContainer modContainer) {
         logger.debug("CommandConfirm reached construction");
